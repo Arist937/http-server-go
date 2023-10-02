bc734bfe71e7aafb7b8f28076780fa783f5d46c4
Arist937 <Aristotle937@gmail.com>
2023-10-02 15:41:32 -0400

    pass stage 2

--- app/server.go
+++ app/server.go
@@ -13,9 +13,15 @@ func main() {
 		os.Exit(1)
 	}
 	
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	buffer := make([]byte, 1024)
+	conn.Read(buffer[:])
+
+	data := []byte("HTTP/1.1 200 OK\r\n\r\n")
+	conn.Write(data)
 }
