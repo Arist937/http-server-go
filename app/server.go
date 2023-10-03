2480e4a01cbfda37a6acad114163eb3f05521886
Arist937 <Aristotle937@gmail.com>
2023-10-02 21:12:46 -0400

    pass stage 5

--- app/server.go
+++ app/server.go
@@ -31,24 +31,45 @@ func main() {
 	start_line := strings.Split(request[0], " ")
 	path := start_line[1]
 
+	header_map := make(map[string]string)
+	for _, line := range request[2:] {
+		header := strings.Split(line, ": ")
+
+		if len(header) > 1 {
+			header_map[header[0]] = header[1]	
+		}
+	}
+
+	fmt.Println(header_map)
+
+	// Default response
 	response := []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+
+	// Handle different requests to HTTP server
 	if path == "/" {
 		response = []byte("HTTP/1.1 200 OK\r\n\r\n")
 	} else {
-		blocks := strings.Split(path, "/")
-		if len(blocks) > 2 {
-			req_type := blocks[1]
-			req_str := strings.Join(blocks[2:], "/")
+		blocks := strings.Split(path, "/")[1:]
+		fmt.Println(blocks)
+		req_type := blocks[0]
 
-			if req_type == "echo" {
-				data := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(req_str), req_str)
-				response = []byte(data)
-			}
+		if req_type == "echo" {
+			body := strings.Join(blocks[1:], "/")
+			response = generateResponse(body)
+		} else if req_type == "user-agent" {
+			response = generateResponse(header_map["User-Agent"])
 		}
 	}
 
 	conn.Write(response)
 
+	// Close the connection when done
 	conn.Close()
 	l.Close()
+}
+
+func generateResponse(body string) []byte {
+	response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(body), body)
+	fmt.Println(response)
+	return []byte(response)
 }
